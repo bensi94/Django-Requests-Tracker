32f6d7eb3a8d4e0bc70869c3676ea74bb1f51e13
Benedikt Óskarsson <bensi94@hotmail.com>
2022-12-05 23:07:12 +0000

    Add requests sorting

--- requests_tracker/views.py
+++ requests_tracker/views.py
@@ -1,32 +1,77 @@
+from datetime import datetime
+from typing import Literal
 from uuid import UUID
 
 from django.template.response import TemplateResponse
 
+from requests_tracker.main_request_collector import MainRequestCollector
 from requests_tracker.middleware import RequestWithCollectors
+
+RequestsType = dict[UUID, MainRequestCollector]
 
 
 def is_htmx_request(request: RequestWithCollectors) -> bool:
     return request.headers.get("HX-Request", "").lower() == "true"
 
 
-def is_htmx_search_query(request: RequestWithCollectors) -> bool:
+def is_htmx_search_or_sort_request(request: RequestWithCollectors) -> bool:
     return request.headers.get("HX-Target") == "request-list-search-results"
 
 
+def sort_requests(
+    requests: RequestsType,
+    requests_sorter: str,
+    requests_direction: str,
+) -> RequestsType:
+    def sort_func(
+        item: tuple[UUID, MainRequestCollector]
+    ) -> str | int | datetime | None:
+        _, request = item
+        match requests_sorter:  # noqa: E999 (ruff does not recognise pattern matching)
+            case "time":
+                return request.start_time
+            case "duration":
+                return request.duration
+            case "name":
+                return str(request.request.path)
+            case "view":
+                return request.django_view
+            case "query_count":
+                return request.sql_collector.num_queries
+            case "duplicate_query_count":
+                return request.sql_collector.total_duplicate_queries
+            case "similar_query_count":
+                return request.sql_collector.total_similar_queries
+            case _:
+                return request.start_time
+
+    reverse = requests_direction != "ascending"
+    return dict(
+        sorted(requests.items(), key=sort_func, reverse=reverse)  # type: ignore
+    )
+
+
 def index(request: RequestWithCollectors) -> TemplateResponse:
-    search_filter = request.GET.get("requests_filter", "")
+    requests_filter = request.GET.get("requests_filter", "")
+    requests_sorter = request.GET.get("requests_sorter", "time")
+    requests_direction = request.GET.get("requests_direction", "")
+
+    sorted_requests = sort_requests(
+        request.request_collectors,
+        requests_sorter,
+        requests_direction,
+    )
 
     requests = {
         request_id: request_collector.get_as_context()
-        for request_id, request_collector in reversed(
-            list(request.request_collectors.items())
-        )
-        if not search_filter or request_collector.matches_search_filter(search_filter)
+        for request_id, request_collector in list(sorted_requests.items())
+        if not requests_filter
+        or request_collector.matches_search_filter(requests_filter)
     }
 
     template = "index.html"
 
-    if is_htmx_search_query(request):
+    if is_htmx_search_or_sort_request(request):
         template = "partials/request_list_only_partial.html"
     elif is_htmx_request(request):
         template = "partials/request_list_partial.html"
@@ -34,7 +79,12 @@ def index(request: RequestWithCollectors) -> TemplateResponse:
     return TemplateResponse(
         request,
         template,
-        context={"requests": requests, "current_search": search_filter},
+        context={
+            "requests": requests,
+            "requests_filter": requests_filter,
+            "requests_sorter": requests_sorter,
+            "requests_direction": requests_direction,
+        },
     )
 
 
